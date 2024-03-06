b686d1c4895eec9cfa180e2211a959cf64ea9b8d
ashwith26 <ashwith26@gmail.com>
2024-03-06 13:26:57 +0530

    configuration modifiied

--- main.tf
+++ main.tf
@@ -1,18 +1,66 @@
-provider {
-    region = "ap-south-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "my_instance" {
-    vpc_id = "vpc-047d33d32428a6a69"
-    ami = "ami-06aa3f7caf3a30282"
-    subnet_id = "subnet-0baa2a3c470ca209f"
-    key_pair = "devopslearning"
-    tags = {
-        Name = "Docker instance"
-        Env = "Dev"
-    }   
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "my-tf-test-bucket"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_subnet" "sub" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "RTA" {
+  subnet_id      = aws_subnet.sub.id
+  route_table_id = aws_route_table.RT.id
+}
+
+resource "aws_security_group" "SG" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-07d9b9ddc6cd8dd30"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.SG.id]
+  subnet_id              = aws_subnet.sub.id
 }
